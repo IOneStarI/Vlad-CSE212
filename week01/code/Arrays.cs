43211a6e6aa5e0c7e7545bb646fd99be9b828c35
Vlad Didkovskiy <vladgreen.vg@gmail.com>
2026-05-05 01:13:57 +0300

    w1 arrays task

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -8,12 +8,17 @@ public static class Arrays
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     public static double[] MultiplesOf(double number, int length)
     {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // Create array with size of length
+        double[] multiples = new double[length];
 
-        return []; // replace this return statement with your own
+        // Loop through the array with multiples of number
+        for (int i = 0; i < length; i++)
+        {
+        // Calculate the multiple
+            multiples[i] = number * (i + 1);
+        }
+        // Return the array
+        return multiples; 
     }
 
     /// <summary>
@@ -25,9 +30,16 @@ public static class Arrays
     /// </summary>
     public static void RotateListRight(List<int> data, int amount)
     {
-        // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // Find the index where we should start rotating
+        int startIndex = data.Count - amount;
+
+        // Get the last 'amount' item in the list
+        List<int> lastItems = data.GetRange(startIndex, amount);
+
+        // Remove the last 'amount' items from the list
+        data.RemoveRange(startIndex, amount);
+
+        // Add the last 'amount' items to the beginning of the list
+        data.InsertRange(0, lastItems);
     }
 }
